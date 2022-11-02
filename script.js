1e82e0993633ae7ab08f7a1a0a15dd91de2cf7ab
Joshua Cadavez <jcadavez@insider.com>
2022-11-02 13:32:52 -0400

    Added some context

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+/**
+ * Ramps the VUs in Stages
+ */
 export const options = {
   stages: [
+    // ramps from 0 to 20 VUs in 30s
     { duration: '30s', target: 20 },
+    // ramps from 20 to 10 VUs in 1m30s
     { duration: '1m30s', target: 10 },
+    // ramps from 10 to 0 VUs in 20s
     { duration: '20s', target: 0 },
   ],
 };
